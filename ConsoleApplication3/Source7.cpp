0c4256c6d548318ba473af2807c3ec597c53cc10
it.step.sochi@outlook.com <it.step.sochi@outlook.com>
2022-02-03 20:45:16 +0300

    ff

--- ConsoleApplication3/Source7.cpp
+++ ConsoleApplication3/Source7.cpp
@@ -1,4 +1,4 @@
-#include <iostream>	
+/*#include <iostream>	
 class point
 {
 public:
@@ -7,17 +7,9 @@ public:
 	friend std::ostream& operator<<(std::ostream& output, point&& point);
 	friend point& operator+(point& p1, point& p2);
 	template<typename T>
-	friend point& operator*(point& p1, int& p2);
 
 };
 template <typename T>
-point& operator*(point& p1, int& p2)
-{
-	point p3;
-	p3.x = p1.x * p2;
-	p3.y = p1.y * p2;
-	p3.z = p1.z * p2;
-}
 point& operator+(point& p1, point& p2)
 {
 	point sum;
@@ -32,25 +24,23 @@ std::ostream& operator<<(std::ostream& output, point&& point)
 	output << point.x << "___" << point.y << "___" << point.z<< "\n";
 	return output;
 }
-template<typename T>
-T sumfunkT(T param1, T param2)
+template<typename T, typename T1>
+*/
+/*T sumfunkT(T param1, T param2)
+{
+	return param1 + param2;
+}*/
+/*
+T sumfunkT(T param1, T1 param2)
 {
 	return param1 + param2;
 }
 int sumfunk(int num1, int num2) { return num1 + num2; }
-point sumfunk(point p1, point p2)
-{
-	point sum;
-	sum.x = p1.x + p2.x;
-	sum.x = p1.y + p2.y;
-	sum.x = p1.z + p2.z;
-	return sum;
-}
 int main()
 {
 	std::cout << sumfunkT(3, 5);
 	point p1, p2;
-	p1 * (int)3;
-	std::cout << ;
+	std::cout << sumfunkT(5.10, 44) << std::endl;
+	std::cout << sumfunkT(5.10, 4)<< std::endl;
 	return 0;
-}
+}*/
---
+++ ConsoleApplication3/Source8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+template<class T>
+class point
+{
+public:
+	T x, y, z;
+	point() :x{ 1 }, y{ 1 }, z{ 1 }{}
+	point(T num) :x{num},y{num}, z{num}{}
+	void printinfo()
+	{
+		std::cout << x << ' ' << y << ' ' << z <<std::endl;
+	}
+	template <class T>
+	friend std::ostream& operator<<(std::ostream& output, point<T>& param);
+};
+template<class T>
+std::ostream& operator<<(std::ostream& output,point<T>& param)
+{
+	output << param.x << ' ' << param.y << ' ' << param.z << std::endl;
+	return output;
+}
+int main()
+{
+//	std::vector<int> vect;
+	point<char> p1(2.5); //ТО жесамое что сверху почти
+	std::cout << p1;
+	return 0;
+
+}
